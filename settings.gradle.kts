e5b41d761f6f84a00ba7bee4f5bd19a2b53f6f8c
M Jamshed <jamshedxd37@gmail.com>
2024-03-31 05:03:22 +0500

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "restaurant_detail_project"
+include(":app")
